614478f2891821f87ba529563a4f1570948aa710
shaneostonstowe <sstowe@jbookingbug.com>
2021-07-05 18:42:01 -0700

    moves models t0 seperate file, adds num_upcoming_ to venues

--- app.py
+++ app.py
@@ -6,15 +6,11 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
-
 from forms import *
+from models import *
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -26,59 +22,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-
-
-# ----------------------------------------------------------------------------#
-# Models.
-# ----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column('genres', db.ARRAY(db.String()), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(255))
-    shows = db.relationship('Show', backref='venue')
-
-    def __repr__(self):
-        return f'<Venue {self.id} name: {self.name}>'
-
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(255))
-
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer)
-    # venue_id = db.Column(db.Integer)
-    start_time = db.Column(db.DateTime)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-
 
 # ----------------------------------------------------------------------------#
 # Filters.
@@ -110,7 +53,8 @@ def index():
 
 @app.route('/venues')
 def venues():
-    venues_by_city = [];
+    venues_by_city = []
+    today = datetime.now
 
     venues = Venue.query.all()
 
@@ -127,16 +71,23 @@ def venues():
         })
 
     for venue in venues:
-        # TODO add upcoming shows
+
+        num_upcoming_shows = 0
+        shows = Show.query.filter_by(venue_id=venue.id).all()
+        for show in shows:
+            if show.start_time > today:
+                num_upcoming_shows += 1
+
         for location in venues_by_city:
             if venue.city == location['city'] and venue.state == location['state']:
                 location['venues'].append({
                     'id': venue.id,
-                    'name': venue.name
+                    'name': venue.name,
+                    'num_upcoming_shows': num_upcoming_shows
                 })
 
 
-    return render_template('pages/venues.html', areas=venues_by_city);
+    return render_template('pages/venues.html', areas=venues_by_city)
 
 
 @app.route('/venues/search', methods=['POST'])
---
+++ models.py
@@ -0,0 +1,62 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column('genres', db.ARRAY(db.String()), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(255))
+    shows = db.relationship('Show', backref='venue')
+
+    def __repr__(self):
+        return f'<Venue {self.id} name: {self.name}>'
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(255))
+
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer)
+    # venue_id = db.Column(db.Integer)
+    start_time = db.Column(db.DateTime)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
